b066f1ce52ad572bbdab628a28058f6283c1198a
nico-Zero <zandaxheart955@gmail.com>
2024-01-04 18:29:21 +0530

    complte.

--- Blog_Site/settings.py
+++ Blog_Site/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import mimetypes
 import dj_database_url
 from pathlib import Path
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,6 +80,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "Blog_Site.wsgi.application"
+
+mimetypes.add_type("text/css", ".css", True)
 
 
 # Database

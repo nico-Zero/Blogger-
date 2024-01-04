d4f472cc2bc92594e3562325eef734d656cc095a
nico-Zero <zandaxheart955@gmail.com>
2024-01-04 14:41:01 +0530

    complte.

--- Blog_Site/settings.py
+++ Blog_Site/settings.py
@@ -10,9 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import dj_database_url
-import os
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-)^$tojj4m&z@lrqvutj5ukj6jyu1@56_i-g8o*l#c_rh8-%$td"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ") #type: ignore
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -91,8 +88,6 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get("DATABASES_URL")
-DATABASES["default"] = dj_database_url.parse(database_url) #type: ignore
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

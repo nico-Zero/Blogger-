ba073421976c2d0e79e8beb36c16e56e7668646c
nico-Zero <zandaxheart955@gmail.com>
2024-01-04 20:12:39 +0530

    complte.

--- Blog_Site/settings.py
+++ Blog_Site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition

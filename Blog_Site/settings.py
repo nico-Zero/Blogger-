15f6d8c3bbaa159a44dc7db9b6369df246fe8787
nico-Zero <zandaxheart955@gmail.com>
2024-01-04 19:40:14 +0530

    complte.

--- Blog_Site/settings.py
+++ Blog_Site/settings.py
@@ -57,8 +57,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "Blog_Site.urls"
@@ -138,11 +136,6 @@ STATICFILES_DIRS = [
     / "static"
     / "main",
 ]
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
